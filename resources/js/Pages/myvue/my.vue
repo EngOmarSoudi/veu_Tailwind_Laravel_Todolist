56fc6a309a704baaa85f8b6760ddd8c8c2a65499
EngOmarSoudi <92035173+EngOmarSoudi@users.noreply.github.com>
2023-01-14 10:52:03 +0300

    validation +

--- resources/js/Pages/myvue/my.vue
+++ resources/js/Pages/myvue/my.vue
@@ -221,8 +221,10 @@
                         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
                             Date
                         </label>
-                        <input v-model="date" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="date" placeholder="Doe">
-                        <p v-show="taskErorrs.date" class="text-red-500 text-xs italic">Please fill out this field.</p>
+                        <vee-field v-model="date" name="date" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="date" placeholder="Doe"/>
+<!--                            <vee-field  v-model="date" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="date" placeholder="Doe" />-->
+<!--                            <p v-show="taskErorrs.date" class="text-red-500 text-xs italic">Please fill out this field.</p>-->
+                            <error-message name="date"  class="text-red-500 text-xs italic"></error-message>
                         </div>
                     </div>
                     <div class="flex flex-wrap -mx-3 mb-6">
@@ -317,7 +319,7 @@ export default {
             schema:{
                 title: 'required',
                 time: 'required',
-                date: 'required',
+                date: 'required:between:2022-12-04,2042-12-04',
                 details: 'required',
 
             },
--- resources/js/includes/validation.js
+++ resources/js/includes/validation.js
@@ -1,10 +1,14 @@
 import { Form as veeForm,Field as veeFieled, defineRule , ErrorMessage} from "vee-validate";
-import {required} from "@vee-validate/rules";
+import {required , min , max , alpha_spaces as alphaSpaces , between  } from "@vee-validate/rules";
 export default{
     install(app){
         app.component('vee-form',veeForm);
         app.component('vee-field',veeFieled);
         app.component( "ErrorMessage", ErrorMessage );
-        defineRule('required',required)
+        defineRule('required',required);
+        defineRule('min',min);
+        defineRule('max',max);
+        defineRule('alpha_spaces',alphaSpaces);
+        defineRule('between',between);
     },
 }
